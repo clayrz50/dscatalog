30a4f1a77cc7ad63a87e823fb5afcf5cad4ead92
Clayton <clayrz50@gmail.com>
2024-02-21 19:20:07 -0300

    Error AnnotationConfigContextLoaderUtils

---
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,69 @@
+package com.devsuperior.dscatalog.repositories;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.tests.Factory;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+	@Autowired
+	private ProductRepository repository;
+
+	private Long existingId;
+	
+	private Long nonExistingId;
+
+	private Long countTotalProduct;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+		countTotalProduct = 25L;
+		nonExistingId=1000L;
+	}
+
+	@Test
+	public void saveShouldPersistWithAutoIncrementWhenIdIsNull() {
+		Product product = Factory.createProduct();
+		product.setId(null);
+		product = repository.save(product);
+
+		Assertions.assertNotNull(product.getId());
+		Assertions.assertEquals(countTotalProduct + 1, product.getId());
+	}
+
+	@Test
+	public void deleteShouldDeleteObjectWhenIdExists() {
+
+		repository.deleteById(existingId);
+
+		Optional<Product> result = repository.findById(existingId);
+
+		Assertions.assertFalse(result.isPresent());
+	}
+
+	@Test
+	public void findByIdShouldReturnNonEmptyOptionalProductWhenIdExists() {
+
+		Optional<Product> result = repository.findById(existingId);
+		
+		Assertions.assertTrue(result.isPresent());
+	}
+	
+	@Test
+	public void findByIdShouldReturnEmptyOptionalProductWhenIdIsNotExists() {
+
+		Optional<Product> result = repository.findById(nonExistingId);
+		
+		Assertions.assertTrue(result.isEmpty());
+	}
+
+}
